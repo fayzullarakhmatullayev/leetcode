8d84fd66d26da1e6ce9d6a2749b9aa0fab1e2aef
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-08-16 10:23:18 +0500

    done: compactObject

--- medium/compactObject.js
+++ medium/compactObject.js
@@ -1,30 +1,15 @@
-// 114 tests has been passed out of 156
-
 const compactObject = (obj) => {
-  const isArray = Array.isArray(obj);
-  if (isArray) {
-    let result = [];
-    for (let i = 0; i < obj.length; i++) {
-      if (Array.isArray(obj[i])) {
-        result.push(compactObject(obj[i]));
-      } else if (obj[i]) {
-        result.push(obj[i]);
+  if (Array.isArray(obj)) {
+    return obj.map(compactObject).filter(Boolean);
+  } else if (obj !== null && typeof obj === 'object') {
+    return Object.keys(obj).reduce((acc, key) => {
+      const compactedValue = compactObject(obj[key]);
+      if (Boolean(compactedValue)) {
+        acc[key] = compactedValue;
       }
-    }
-    return result;
+      return acc;
+    }, {});
   } else {
-    let res = {};
-    for (let key in obj) {
-      if (obj[key]) {
-        const val =
-          typeof obj[key] === 'string' ||
-          typeof obj[key] === 'number' ||
-          typeof obj[key] === 'boolean'
-            ? obj[key]
-            : compactObject(obj[key]);
-        res = { ...res, [key]: val };
-      }
-    }
-    return res;
+    return obj;
   }
 };

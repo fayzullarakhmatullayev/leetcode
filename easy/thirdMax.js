207139bc64047519553ba517ceb1b12665b2713e
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2024-08-18 00:17:59 +0500

    pending: thirdMax

---
+++ easy/thirdMax.js
@@ -0,0 +1,7 @@
+var thirdMax = function (nums) {
+  for (let i = 0; i < nums.length; i++) {}
+};
+
+console.log(thirdMax([3, 2, 1])); // 1
+console.log(thirdMax([1, 2])); // 2
+console.log(thirdMax([2, 2, 3, 1])); // 1

a8604dc9a0ffe2d6fa42797fb0c456cea34e7288
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2024-08-23 01:46:28 +0500

    add palindrome best solution for learning purpose

--- easy/isPalindrome.js
+++ easy/isPalindrome.js
@@ -7,6 +7,30 @@ var isPalindrome = function (s) {
   return str === str.split('').reverse().join('');
 };
 
+// best solution
+// var isPalindrome = function (s) {
+//   let left = 0;
+//   let right = s.length - 1;
+
+//   while (left < right) {
+//     while (left < right && !isAlphanumeric(s[left])) left++;
+//     while (left < right && !isAlphanumeric(s[right])) right--;
+
+//     if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+//       return false;
+//     }
+
+//     left++;
+//     right--;
+//   }
+
+//   return true;
+// };
+
+// function isAlphanumeric(c) {
+//   return /[a-z0-9]/i.test(c);
+// }
+
 console.log(isPalindrome('A man, a plan, a canal: Panama')); //true
 console.log(isPalindrome('race a car')); // false
 console.log(isPalindrome(' ')); // true

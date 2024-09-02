413f1c8f4cd82bbdd7748abfc7be102ef88cc8a2
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2024-09-02 23:09:47 +0500

    done: fibonacci

---
+++ easy/fib.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n, memo = {}) {
+  if (n in memo) {
+    return memo[n];
+  }
+  if (n <= 1) return n;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+  return memo[n];
+};
+
+console.log(fib(4));

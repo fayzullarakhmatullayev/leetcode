62418c2e52b5fa12a152ffb358d2c986a9b214c5
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2024-09-05 01:06:17 +0500

    updated

--- easy/isLongPressedName.js
+++ easy/isLongPressedName.js
@@ -23,5 +23,25 @@ var isLongPressedName = function (name, typed) {
   return true;
 };
 
+// correct answer
+// var isLongPressedName = function (name, typed) {
+//   let i = 0; // Pointer for `name`
+//   let j = 0; // Pointer for `typed`
+
+//   while (j < typed.length) {
+//     if (i < name.length && name[i] === typed[j]) {
+//       // If characters match, move both pointers
+//       i++;
+//     } else if (j === 0 || typed[j] !== typed[j - 1]) {
+//       // If characters don't match and `typed[j]` is not a long-pressed version of `typed[j-1]`
+//       return false;
+//     }
+//     j++;
+//   }
+
+//   // If all characters in `name` have been matched, return true
+//   return i === name.length;
+// };
+
 console.log(isLongPressedName('alex', 'aaleex')); // true
 console.log(isLongPressedName('saeed', 'ssaaedd')); // false

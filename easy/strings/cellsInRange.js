98cefd06758795f48e31816bceabe1be1e3b19d2
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-09-25 12:32:15 +0500

    feat: cellsInRange

--- easy/strings/cellsInRange.js
+++ easy/strings/cellsInRange.js
@@ -2,7 +2,23 @@
  * @param {string} s
  * @return {string[]}
  */
-var cellsInRange = function (s) {};
+var cellsInRange = function (s) {
+  const fromChar = s[0].charCodeAt();
+  const toChar = s[3].charCodeAt();
+
+  const fromDigit = +s[1];
+  const toDigit = +s[4];
+
+  const result = [];
+
+  for (let i = fromChar; i <= toChar; i++) {
+    for (let j = fromDigit; j <= toDigit; j++) {
+      result.push(String.fromCharCode(i) + j);
+    }
+  }
+
+  return result;
+};
 
 console.log(cellsInRange('K1:L2'));
 

700cf4daacde586e1333ad55d23c033a7a5f5f5b
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-10-06 23:21:38 +0500

    add: gitignore

---
+++ medium/strings/checkInclusion.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const checkInclusion = function (s1, s2) {
+  let count = 0;
+
+  console.log("hello");
+};
+
+console.log(checkInclusion("ab", "eidbaooo")); // true
+console.log(checkInclusion("ab", "eidboaoo")); // false
+
+// https://leetcode.com/problems/permutation-in-string/?envType=daily-question&envId=2024-10-05

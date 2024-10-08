8a866108d6ee99f98f8fbeb8e50f0f4987113392
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2024-10-09 01:40:04 +0500

    feat: #easy 1 string, 1array

--- easy/strings/equalFrequency.js
+++ easy/strings/equalFrequency.js
@@ -3,19 +3,32 @@
  * @return {boolean}
  */
 var equalFrequency = function (word) {
-  let count = 0;
+  const map = new Map();
 
-  for (let i = 0; i < word.length; i++) {
-    if (word[i + 1] && word[i] === word[i + 1]) {
-      count++;
-    }
-    if (count > 1) return false;
+  for (let w of word) {
+    map.set(w, map.get(w) + 1 || 1);
   }
 
-  return true;
+  const freqArr = Array.from(map.values());
+
+  for (let i = 0; i < freqArr.length; i++) {
+    let tempArr = [...freqArr];
+    tempArr[i] -= 1;
+
+    const remainingFrequencies = tempArr.filter((f) => f > 0);
+    const uniqueFreq = new Set(remainingFrequencies);
+
+    if (uniqueFreq.size === 1) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
-console.log(equalFrequency('abcc')); // true
-console.log(equalFrequency('aazz')); // true
+// console.log(equalFrequency('abcc')); // true
+// console.log(equalFrequency('aazz')); // false
+// console.log(equalFrequency('adbc')); // true
+console.log(equalFrequency('abbcc')); // true
 
 // https://leetcode.com/problems/remove-letter-to-equalize-frequency/description/

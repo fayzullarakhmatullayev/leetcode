d476bae551cdb226fe7ececeb9d7ee220b31a22f
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-11-09 03:23:38 +0500

    feat: easy 1 string

---
+++ easy/arrays/countStudents/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+
+var countStudents = function (students, sandwiches) {
+  while (students.length > 0) {
+    if (!students.includes(sandwiches[0])) break;
+
+    if (students[0] !== sandwiches[0]) {
+      students.push(students.shift());
+    } else {
+      students.shift();
+      sandwiches.shift();
+    }
+  }
+  return students.length;
+};
+console.log(countStudents([1, 1, 0, 0], [0, 1, 0, 1])); // 0
+console.log(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1])); // 3

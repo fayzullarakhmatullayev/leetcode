156f3c359563b67b98cfd2d8a5f5e09f5f900f6f
Fayzulla <fayzullarakhmatullayev@gmail.com>
2024-12-14 10:47:49 +0500

    feat: 1 medium array

---
+++ medium/arrays/sortTheStudents.ts
@@ -0,0 +1,34 @@
+type MapType = {
+  index: number;
+  value: number;
+};
+
+function sortTheStudents(score: number[][], k: number): number[][] {
+  const map: MapType[] = [];
+
+  for (let i: number = 0; i < score.length; i++) {
+    map.push({ index: i, value: score[i][k] });
+  }
+
+  map.sort((a: MapType, b: MapType) => b.value - a.value);
+
+  const answer: number[][] = Array(score.length);
+
+  for (let i = 0; i < score.length; i++) {
+    const mapIdx = map[i].index;
+    answer[i] = score[mapIdx];
+  }
+
+  return answer;
+}
+
+console.log(
+  sortTheStudents(
+    [
+      [10, 6, 9, 1],
+      [7, 5, 11, 2],
+      [4, 8, 3, 15]
+    ],
+    2
+  )
+); // [ [ 7, 5, 11, 2 ], [ 10, 6, 9, 1 ], [ 4, 8, 3, 15 ] ]

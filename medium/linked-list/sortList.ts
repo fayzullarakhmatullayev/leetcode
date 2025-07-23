c5455065917d3573aefe92f26e3febba26db09cd
Fayzullo <fayzullarakhmatullayev@gmail.com>
2025-07-24 01:15:36 +0500

    feat: implement sortColors function to sort colors in-place using counting sort

---
+++ medium/linked-list/sortList.ts
@@ -0,0 +1,40 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function sortList(head: ListNode | null): ListNode | null {
+  const nums: number[] = [];
+  let curr = head;
+
+  while (curr) {
+    nums.push(curr.val);
+    curr = curr.next;
+  }
+
+  nums.sort((a, b) => a - b);
+
+  let i = 0;
+  curr = head;
+
+  while (curr) {
+    curr.val = nums[i];
+    curr = curr.next;
+    i++;
+  }
+
+  return head;
+}
+
+const values = [4, 2, 1, 3];
+let head: ListNode | null = null;
+
+for (let i = values.length - 1; i >= 0; i--) {
+  head = new ListNode(values[i], head);
+}
+
+console.log(JSON.stringify(sortList(head), null, 4));

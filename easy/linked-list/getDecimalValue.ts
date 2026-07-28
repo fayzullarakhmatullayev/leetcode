5ec386faaf2fc0a9562dee6e613d3788803cd44f
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2026-07-28 12:35:41 +0500

    refactor: streamline getDecimalValue function and remove unnecessary class definition

--- easy/linked-list/getDecimalValue.ts
+++ easy/linked-list/getDecimalValue.ts
@@ -1,28 +1,15 @@
-function decimalValueSolution() {
-  class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null) {
-      this.val = val === undefined ? 0 : val;
-      this.next = next === undefined ? null : next;
-    }
+import { createList, ListNode } from './utils';
+
+function getDecimalValue(head: ListNode | null): number {
+  let curr: ListNode | null = head;
+  const vals: number[] = [];
+
+  while (curr) {
+    vals.push(curr.val);
+    curr = curr.next;
   }
 
-  function getDecimalValue(head: ListNode | null): number {
-    let curr: ListNode | null = head;
-    const vals: number[] = [];
-
-    while (curr) {
-      vals.push(curr.val);
-      curr = curr.next;
-    }
-
-    return parseInt(vals.join(''), 2);
-  }
-
-  const node = new ListNode(1, new ListNode(0, new ListNode(1)));
-
-  console.log(getDecimalValue(node));
+  return parseInt(vals.join(''), 2);
 }
 
-decimalValueSolution();
+console.log(getDecimalValue(createList([1, 0, 1])));

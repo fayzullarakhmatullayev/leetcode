91c39002b3570e4f5001995d340a83434a43f2ca
Fayzullo <fayzullarakhmatullayev@gmail.com>
2026-07-29 13:38:07 +0500

    feat: implement removeElements function to remove specified values from linked list

---
+++ easy/linked-list/removeElements.ts
@@ -0,0 +1,25 @@
+import { stringify, createList, ListNode } from './utils';
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+
+  let prev = dummy;
+  let current = head;
+
+  while (current) {
+    if (current.val === val) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+
+    current = current.next;
+  }
+
+  return dummy.next;
+}
+
+console.log('result: ', stringify(removeElements(createList([1, 2, 6, 3, 4, 5, 6]), 6))); // [1,2,3,4,5]
+console.log('result:', stringify(removeElements(createList([6, 2, 6, 3, 6]), 6))); // [1,2,3,4,5]
+console.log('result: ', stringify(removeElements(createList([7, 7, 7, 7, 7]), 7))); // [1,2,3,4,5]
